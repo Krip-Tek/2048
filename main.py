86f83e29d6d351ad8feb154b41ccd7976d5888fc
Andrey Krip <oreal404@mail.ru>
2023-03-18 11:25:08 +0400

    add display and events loop

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+import pygame
+
+pygame.init()
+
+
+screen = pygame.display.set_mode((800, 600))  # (ширина, высота)
+
+while True:
+
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            exit()
+
+    pygame.display.update()
